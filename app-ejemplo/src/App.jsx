3cd6810de7df7c3d5894537466f0233844ac8724
Oscar Galvez <oscar.galvez27@outlook.com>
2021-11-10 01:00:17 -0600

    first commit

---
+++ app-ejemplo/src/App.jsx
@@ -0,0 +1,5 @@
+const Title = () => {
+  return <h1>Hola Koder desde react!</h1>;
+};
+
+export default Title;
---
+++ app-ejemplo/src/index.js
@@ -0,0 +1,7 @@
+import ReactDom from "react-dom";
+import Title from "./App";
+
+const root = document.getElementById("root");
+//const title = <h1>Hola Koder desde react!</h1>;
+
+ReactDom.render(<Title />, root);
